32b7dade6777bae7472b1f8afa4b12ea964a6904
Johnny Willian <johnnywillian@live.com>
2023-12-02 15:31:31 -0300

    Salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+// Converter dados do formulário em objeto JavaScript
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// Rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
